5e2ebb59c4894c8be54b220cf9edb43f8260cb0f
Cagri Esen <esencgr@gmail.com>
2021-05-14 00:09:02 +0300

    initial

---
+++ poll/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Poll(models.Model):
+    question = models.TextField()
+    option_one = models.CharField(max_length=30)
+    option_two = models.CharField(max_length=30)
+    option_three = models.CharField(max_length=30)
+    option_one_count = models.IntegerField(default=0)
+    option_two_count = models.IntegerField(default=0)
+    option_three_count = models.IntegerField(default=0)
+
+    def total(self):
+        return self.option_one_count + self.option_two_count + self.option_three_count
+
+    def __str__(self):
+        return self.question
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
---
+++ poll/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render, redirect, Http404
+from django.http import HttpResponse, Http404
+from .forms import CreatePollForm, CreateUserForm
+from .models import Poll, User
+
+def signup(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CreateUserForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'poll/signup.html', context)
+
+def home(request):
+    polls = Poll.objects.all()
+    context = {
+        'polls' : polls
+    }
+    return render(request, 'poll/home.html', context)
+
+def create(request):
+    if not request.user.is_authenticated:
+        raise Http404
+
+    if request.method == 'POST':
+        form = CreatePollForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CreatePollForm()
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'poll/create.html', context)
+
+def update(request, poll_id):
+    if not request.user.is_authenticated:
+        raise Http404
+
+    poll = Poll.objects.get(pk=poll_id)
+    form = CreatePollForm(request.POST or None, instance=poll) 
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'poll/create.html', context)
+
+def delete(request, poll_id):
+    if not request.user.is_authenticated:
+        raise Http404
+    
+    poll = Poll.objects.get(pk=poll_id)
+    poll.delete( )
+    return redirect('home')
+ 
+def vote(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+    if request.method == 'POST':
+        selected_option = request.POST['poll']
+        if selected_option == 'option1':
+            poll.option_one_count += 1
+        elif selected_option == 'option2':
+            poll.option_two_count += 1
+        elif selected_option == 'option3':
+            poll.option_three_count += 1
+        else:
+            return HttpResponse(400, 'Invalid forz')
+        poll.save()
+        return redirect('results', poll.id)
+
+    context = {
+        'poll' : poll
+    }
+    return render(request, 'poll/vote.html', context)
+
+def results(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+    context = {
+        'poll' : poll
+    }
+    return render(request, 'poll/results.html', context)
