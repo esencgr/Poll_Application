13857bf2811220fbde6a34fa639ae93a21bb7433
Cagri Esen <esencgr@gmail.com>
2021-05-14 13:33:33 +0300

    crispy forms added

--- user/views.py
+++ user/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm 
+from .forms import RegisterForm, LoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate     
 from django.contrib import messages
 
 def register(request):
     form = RegisterForm(request.POST or None)
-   
+
     if form.is_valid():
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
@@ -26,21 +26,28 @@ def register(request):
     return render(request, "register.html", context)
 
 def loginUser(request):
-    # form = RegisterForm(request.POST or None)
+    form = LoginForm(request.POST or None)
 
-    # context = {
-    #     'form' : form,
-    # }
+    context = {
+        'form' : form,
+    }
     
-    # if form.is_valid():
-    #     username = form.cleaned_data.get('username')
-    #     password = form.cleaned_data.get('password')
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
         
-    #     # database query    
-    #     authenticate(username = username, password = password)
+        # database query    
+        user = authenticate(username = username, password = password) 
 
-    #     if user is None 
-    return render(request, "login.html")
+        if user is None:
+            messages.info(request,"Username or password is incorrect.Try again.")
+            return render(request, "login.html", context)
+        
+        messages.success(request,"Login Successful..")
+        login(request,user)
+        return redirect("home")
     
+    return render(request, "login.html", context)
+
 def logoutUser (request):
     return render(request, "logout.html")
