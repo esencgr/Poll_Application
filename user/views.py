7394b3560d6af366c58a8f1247811e2b6628c344
Cagri Esen <esencgr@gmail.com>
2021-05-17 17:25:53 +0300

    modified

--- user/views.py
+++ user/views.py
@@ -2,34 +2,30 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm, LoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
-from django.contrib import messages
+from django.contrib import messages, auth
 
-
-
-# def login(request):
-#     return render(request, 'login.html')
-
-    
 def register(request):
     form = RegisterForm(request.POST or None)
 
     if form.is_valid():
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
-        
-        newUser = User(username = username)
-        newUser.set_password(password)      # encrypted
 
-        newUser.save()
-        # login(request,newUser)
-        messages.success(request,"Registration Successful..")
-        return redirect("login")
+        if User.objects.filter(username = username).exists():
+            messages.info(request,"User name is already exists ")
+            return redirect("register") 
+        else:
+            newUser = User(username = username)
+            newUser.set_password(password)      # encrypted
+            newUser.save()
+            login(request,newUser)
+            messages.success(request,"Registration Successful..")
+            return redirect("login")
 
     context = {
         'form' : form,
     }
     return render(request, "register.html", context)
-
 
 def loginUser(request):
     form = LoginForm(request.POST or None)
@@ -44,15 +40,18 @@ def loginUser(request):
 
         # database query    
         user = authenticate(username = username, password = password) 
-        if user is None:
+
+        if user is not None:
+            auth.login(request,  user)
+            messages.success(request,"Login Successful..")
+            return redirect("home")
+
+        else:
             messages.info(request,"Username or password is incorrect.Try again.")
             return render(request, "login.html", context)
 
-        messages.success(request,"Login Successful..")
-        login(request,user)
-        return redirect("home")
-
     return render(request, "login.html", context)
+
 
 def logoutUser ( request):
     logout(request)
