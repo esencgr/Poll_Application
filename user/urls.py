5795842555fa78d47fdc2574bcf4111dbc535447
Cagri Esen <esencgr@gmail.com>
2021-05-17 15:20:49 +0300

    added login logout register links

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views 
 
-app_name = 'user'
 
 urlpatterns = [
     path('register/', views.register, name = 'register'),
--- user/views.py
+++ user/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, LoginForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate, logout     
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 
+
+
+# def login(request):
+#     return render(request, 'login.html')
+
+    
 def register(request):
     form = RegisterForm(request.POST or None)
 
@@ -22,31 +28,30 @@ def register(request):
     context = {
         'form' : form,
     }
-
     return render(request, "register.html", context)
+
 
 def loginUser(request):
     form = LoginForm(request.POST or None)
 
     context = {
         'form' : form,
-    }
-    
+    } 
+
     if form.is_valid():
         username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        
+        password = form.cleaned_data.get('password')  
+
         # database query    
         user = authenticate(username = username, password = password) 
-
         if user is None:
             messages.info(request,"Username or password is incorrect.Try again.")
             return render(request, "login.html", context)
-        
+
         messages.success(request,"Login Successful..")
         login(request,user)
         return redirect("home")
-    
+
     return render(request, "login.html", context)
 
 def logoutUser ( request):
